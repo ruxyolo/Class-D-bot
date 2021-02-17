b1db91d84cc1198ebbef2a473ec46fbf28b1d180
ruxyolo <johabaltha@gmail.com>
2021-02-17 15:49:56 +0100

    9

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
-const client = new Discord.Client()
+const intents = ["GUILDS", "GUILD_MEMBERS"];
+const client = new Discord.Client({ intents: intents, ws: { intents: intents } });
 
 
 let CDnum = 0006
